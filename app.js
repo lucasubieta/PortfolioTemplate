5c71e2d4f3f0d54503cbebc8211e4e8789563507
lucasubi <lucasubieta@gmail.com>
2023-04-23 20:33:35 -0300

    main

---
+++ app.js
@@ -0,0 +1,7 @@
+const computerChoice = document.getElementById('computer-choice')
+const userChoice = document.getElementById('user-choise')
+const resultDisplay = document.getElementById('result')
+
+const possibleChoices = document.querySelectorAll('button')
+
+possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click',)) 
